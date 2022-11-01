e2a2438d463c28432bd2d596f7b6f32e3f392d6e
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-01 16:50:44 +0200

    Add files via upload

---
+++ 01. Lab/01. Inheritance/01. Single Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer programmer1 = new Programmer();
+            Programmer programmer2 = new Programmer("Gosho");
+            Programmer programmer3 = new Programmer(5000, "Petur", 4);
+
+            programmer1.CreateBugs();
+            programmer2.CreateBugs();
+            programmer3.CreateBugs();
+        }
+    }
+}
---
+++ 01. Lab/01. Inheritance/01. Single Inheritance/TeamLead.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Inheritance
+{
+    public class TeamLead
+    {
+
+    }
+}
