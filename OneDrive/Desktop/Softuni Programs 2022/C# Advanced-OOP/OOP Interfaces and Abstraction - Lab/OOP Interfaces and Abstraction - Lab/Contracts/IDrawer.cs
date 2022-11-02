c439177f8c837b99d5621c993d8ab442fb7edf92
sashi <georgievaleks854@gmail.com>
2022-11-02 22:40:49 +0200

    Not finished yet test

---
+++ OneDrive/Desktop/Softuni Programs 2022/C# Advanced-OOP/OOP Interfaces and Abstraction - Lab/OOP Interfaces and Abstraction - Lab/Contracts/IDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Interfaces_and_Abstraction___Lab.Contracts
+{
+    public interface IDrawer
+    {
+        public void WriteLine(string text);
+        public void Write(string text);
+        public void WriteAtPosition(int row, int col, string text);
+
+    }
+}
---
+++ OneDrive/Desktop/Softuni Programs 2022/C# Advanced-OOP/OOP Interfaces and Abstraction - Lab/OOP Interfaces and Abstraction - Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using OOP_Interfaces_and_Abstraction___Lab.Contracts;
+using System;
+
+namespace Abstraction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IDrawer drawer = new ConsoleDrawer();
+
+            drawer.Write("Hello");
+            drawer.WriteAtPosition(14, 53, "Hello cunt"); 
+
+            Circle circle = new Circle();
+            circle.Draw();
+        }
+    }
+}
