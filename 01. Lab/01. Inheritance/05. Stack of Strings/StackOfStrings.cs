5ad6023410e51e69bcb5ec607e4c91391c4ad357
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-02 19:59:37 +0200

    Add files via upload

---
+++ 01. Lab/01. Inheritance/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
