d1c91901e747958bab9bedb78cea1f68adf51ccd
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-03 19:09:14 +0200

    Add files via upload

---
+++ 02. Exercise/02. Encapsulation/04. Pizza Calories/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = null;
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "END") break;
+                
+                try
+                {
+                    string[] tokens = cmd.Split(' ');
+                    string action = tokens[0];
+                    if (action == "Pizza")
+                    {
+                        string pizzaName = tokens[1];
+
+                        pizza = new Pizza(pizzaName);
+                    }
+                    else if (action == "Dough")
+                    {
+
+                        string flourType = tokens[1];
+                        string bakingTechnique = tokens[2];
+                        int weight = int.Parse(tokens[3]);
+
+                        Dough dough = new Dough(flourType, bakingTechnique, weight);
+                        pizza.Dough = dough;
+                    }
+                    else
+                    {
+                        string toppingType = tokens[1];
+                        int weight = int.Parse(tokens[2]);
+
+                        Topping topping = new Topping(toppingType, weight);
+                        pizza.AddTopping(topping);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    return;
+                }
+
+            }
+            Console.WriteLine($"{pizza.Name} - {pizza.TotalPizzaCalories():f2} Calories.");
+        }
+    }
+}
