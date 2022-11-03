87878f53b01c3935b653952ee535fd7c397f72bd
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-03 19:07:33 +0200

    Add files via upload

---
+++ 02. Exercise/02. Encapsulation/01. Class Box Data/Box.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+        public double LateralSurfaceArea()
+        {
+            return 2 * length * height + 2 * width * height;
+        }
+        public double Volume()
+        {
+            return length * width * height;
+        }
+    }
+}
