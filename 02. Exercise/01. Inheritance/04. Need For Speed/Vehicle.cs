32bbe774ac50d3539965bff10209318df0188753
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-03 19:05:37 +0200

    Add files via upload

---
+++ 02. Exercise/01. Inheritance/04. Need For Speed/Vehicle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public double DefaultFuelConsumption { get; set; }
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+            this.DefaultFuelConsumption = 1.25;
+        }
+        public virtual double FuelConsumption { get { return DefaultFuelConsumption; } }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption;
+        }
+    }
+    
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+            this.DefaultFuelConsumption = 8;
+        }
+    }
+    public class CrossMotorcycle : Motorcycle
+    {
+        public CrossMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+    public class Car : Vehicle
+    {
+        public Car(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+            this.DefaultFuelConsumption = 3;
+        }
+    }
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+    }
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+            this.DefaultFuelConsumption = 10;
+        }
+    }
+}
