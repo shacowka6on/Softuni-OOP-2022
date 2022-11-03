d60002740e703634eace26093f087fb30c8c246d
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-03 19:08:47 +0200

    Add files via upload

---
+++ 02. Exercise/02. Encapsulation/03. Shopping Spree/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        public IReadOnlyCollection<Product> Products
+        {
+            get { return products.AsReadOnly(); }
+        }
+        public void AddProduct(Product product)
+        {
+            if (money - product.Cost >= 0)
+            {
+                money -= product.Cost;
+                products.Add(product);
+                Console.WriteLine($"{Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+        }
+    }
+}
---
+++ 02. Exercise/02. Encapsulation/03. Shopping Spree/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get { return cost; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                cost = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
