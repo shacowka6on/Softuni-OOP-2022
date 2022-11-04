9630ccbc5c1d24abacf54a02fd96e4850584a818
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-04 19:14:17 +0200

    Add files via upload

---
+++ 01. Lab/02. Encapsulation/01 .Sort Persons By Name And Age/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
