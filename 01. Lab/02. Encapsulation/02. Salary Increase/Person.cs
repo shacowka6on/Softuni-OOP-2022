9d8796b5efad273450baf568190e92199276a9ec
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-04 19:15:04 +0200

    Add files via upload

---
+++ 01. Lab/02. Encapsulation/02. Salary Increase/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                 Salary += Salary * percentage / 200;
+            }
+            else
+            {
+                 Salary += Salary * percentage / 100;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
