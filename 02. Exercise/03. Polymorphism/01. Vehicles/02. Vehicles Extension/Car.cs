c4b347d664e9c6f6e0c45a8856d3c87a53529e17
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-07 16:28:41 +0200

    Add files via upload

---
+++ 02. Exercise/03. Polymorphism/01. Vehicles/02. Vehicles Extension/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.VehiclesExtension
+{
+    public class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption + 0.9, tankCapacity)
+        {
+
+        }
+    }
+}
