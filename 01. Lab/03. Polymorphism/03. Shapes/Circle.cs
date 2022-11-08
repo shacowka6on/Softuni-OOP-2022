de5c1d6e8daa5de6b6cdb6536ca28e1d304b7ec9
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-08 20:26:24 +0200

    Add files via upload

---
+++ 01. Lab/03. Polymorphism/03. Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override string Draw()
+        {
+            return $"{base.Draw()} {GetType().Name}";
+        }
+    }
+}
---
+++ 01. Lab/03. Polymorphism/03. Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Width + 2 * Height;
+        }
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+        public override string Draw()
+        {
+            return $"{base.Draw()} {GetType().Name}";
+        }
+    }
+}
