c02e62375bfa04b6e62cd23f0fc365ac61dc1e72
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-08 20:25:45 +0200

    Add files via upload

---
+++ 01. Lab/03. Polymorphism/02. Animals/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string FavouriteFood { get; set; }
+        public Animal(string name, string favFood)
+        {
+            Name = name;
+            FavouriteFood = favFood;
+        }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+    }
+}
