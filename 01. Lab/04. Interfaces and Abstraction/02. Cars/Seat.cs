0b7695156bb8f67f6411f8177dbf96909153d3af
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-11 02:05:34 +0200

    Add files via upload

---
+++ 01. Lab/04. Interfaces and Abstraction/02. Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}" + Environment.NewLine
+                + Start() + Environment.NewLine
+                + Stop();
+        }
+    }
+}
---
+++ 01. Lab/04. Interfaces and Abstraction/02. Cars/Tesla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries" + Environment.NewLine +
+                Start() + Environment.NewLine +
+                Stop();
+        }
+    }
+}
