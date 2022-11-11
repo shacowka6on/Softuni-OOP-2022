91a305830aef4ae65f4fbc9a73bb0c6e455463b3
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-11 02:05:05 +0200

    Add files via upload

---
+++ 01. Lab/04. Interfaces and Abstraction/01. Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rectangle = new Rectangle(width, height);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
