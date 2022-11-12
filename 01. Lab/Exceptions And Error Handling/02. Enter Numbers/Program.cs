c9b2009189fd5550627fd1e6bcbf6569631f0090
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-12 23:02:26 +0200

    Add files via upload

---
+++ 01. Lab/Exceptions And Error Handling/02. Enter Numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.EnterNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    if (!numbers.Any())
+                    {
+                        numbers.Add(ReadNumber(1, 100));
+                    }
+                    else
+                    {
+                        numbers.Add(ReadNumber(numbers.Max(), 100));
+                    }
+                }
+                catch(FormatException formatEx)
+                {
+                    Console.WriteLine(formatEx.Message);
+                }
+                catch(ArgumentException argEx)
+                {
+                    Console.WriteLine(argEx.Message);
+                }
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+        static int ReadNumber(int start, int end)
+        {
+            int num;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Invalid Number!");
+            }
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - {end}!");
+            }
+            return num;
+        }
+    }
+}
