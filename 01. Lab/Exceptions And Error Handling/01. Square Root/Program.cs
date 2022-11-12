720ddee5cef27dde1706d2d96e10ea9a0bd27417
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-12 23:02:00 +0200

    Add files via upload

---
+++ 01. Lab/Exceptions And Error Handling/01. Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                else
+                {
+                    Console.WriteLine(Math.Sqrt(number));
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
