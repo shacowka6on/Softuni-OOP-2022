5fd5f310d39b7701f2553704f7ce1cd70f6f5475
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-15 18:35:54 +0200

    Add files via upload

---
+++ 01. Lab/Exceptions And Error Handling/04. Sum Of Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04.SumOfIntegers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int sum = 0;
+            foreach (var item in input)
+            {
+                try
+                {
+                    sum += int.Parse(item);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
