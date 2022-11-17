86db7f0171b0e25cf264d6a3b06e05479bd53a37
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-17 19:24:34 +0200

    Add files via upload

---
+++ 01. Lab/Reflection and Attribute/01. Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        
+        public string StealFieldInfo(string classToInvestigate, params string[] fieldNames)
+        {
+            Type classType = Type.GetType(classToInvestigate);
+            FieldInfo[] classFields = classType.GetFields
+                                      (BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            sb.AppendLine($"Class under investigation: {classToInvestigate}");
+            foreach (FieldInfo fieldInfo in classFields.Where(f => fieldNames.Contains(f.Name)))
+            {
+                sb.AppendLine($"{fieldInfo.Name} = {fieldInfo.GetValue(classInstance)}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
