9c70239cc3bae9d1f8116d0736d4530653d37a87
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-22 20:05:47 +0200

    Add files via upload

---
+++ 01. Lab/Reflection and Attribute/06. Code Tracker/AuthorAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public AuthorAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ 01. Lab/Reflection and Attribute/06. Code Tracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        static void Main()
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
