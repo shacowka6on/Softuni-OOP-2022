b8397cd23b2e0abc702bbebaeff93b54714f2255
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-30 18:05:09 +0200

    Add files via upload

---
+++ 02. Exercise/05. Reflection And Attributes/01. Command Pattern/CommandInterpreter.cs
@@ -0,0 +1,24 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] tokens = args.Split();
+            string cmd = tokens[0] + "Command";
+            string[] value = tokens.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().First(x => x.Name == cmd);
+            ICommand command = Activator.CreateInstance(type) as ICommand;
+
+            return command.Execute(value);
+        }
+    }
+}
---
+++ 02. Exercise/05. Reflection And Attributes/01. Command Pattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+            => $"Hello, {args[0]}";
+        
+    }
+}
